be3d4b001362dc9fa8d3347851ef02f93417b504
muddasar-de <muddasarajmal87@gmail.com>
2022-12-27 00:39:07 +0500

    minor changes with like apikey and error handling.

--- client/script.js
+++ client/script.js
@@ -6,23 +6,19 @@ const chatContainer = document.querySelector('#chat_container');
 
 let loadInterval;
 
-function loader(element) {
+const loader = (element) => {
   element.textContent = '';
-
   loadInterval = setInterval(() => {
-    // Update the text content of the loading indicator
     element.textContent += '.';
-
-    // If the loading indicator has reached three dots, reset it
     if (element.textContent === '....') {
       element.textContent = '';
     }
   }, 300);
-}
+};
 
-function typeText(element, text) {
+const typeText = (element, text) => {
+  console.log('text', text);
   let index = 0;
-
   let interval = setInterval(() => {
     if (index < text.length) {
       element.innerHTML += text.charAt(index);
@@ -31,60 +27,48 @@ function typeText(element, text) {
       clearInterval(interval);
     }
   }, 20);
-}
+};
 
-// generate unique ID for each message div of bot
-// necessary for typing text effect for that specific reply
-// without unique ID, typing text will work on every element
-function generateUniqueId() {
-  const timestamp = Date.now();
+const generateUniqueId = () => {
+  const timeStamp = Date.now();
   const randomNumber = Math.random();
-  const hexadecimalString = randomNumber.toString(16);
+  const hexaDecimalNumber = randomNumber.toString(16);
 
-  return `id-${timestamp}-${hexadecimalString}`;
-}
+  return `id-${timeStamp}-${hexaDecimalNumber}`;
+};
 
-function chatStripe(isAi, value, uniqueId) {
+const chatStripe = (isAi, value, uniqueId) => {
   return `
         <div class="wrapper ${isAi && 'ai'}">
             <div class="chat">
-                <div class="profile">
-                    <img 
-                      src=${isAi ? bot : user} 
-                      alt="${isAi ? 'bot' : 'user'}" 
-                    />
-                </div>
-                <div class="message" id=${uniqueId}>${value}</div>
+            <div class="profile">
+            <img src="${isAi ? bot : user}" alt="${isAi ? 'bot' : 'user'}"/>
             </div>
-        </div>
-    `;
-}
+            <div class="message" id="${uniqueId}">${value}</div>
+            </div>
+        </div>`;
+};
 
 const handleSubmit = async (e) => {
   e.preventDefault();
-
   const data = new FormData(form);
 
-  // user's chatstripe
+  // user chat stripe
   chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
-
-  // to clear the textarea input
   form.reset();
 
-  // bot's chatstripe
+  // bot's chatStripe
   const uniqueId = generateUniqueId();
-  chatContainer.innerHTML += chatStripe(true, ' ', uniqueId);
 
-  // to focus scroll to the bottom
+  chatContainer.innerHTML += chatStripe(true, ' ', uniqueId);
   chatContainer.scrollTop = chatContainer.scrollHeight;
 
-  // specific message div
   const messageDiv = document.getElementById(uniqueId);
 
-  // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:5000/', {
+  // fetching data
+  const response = await fetch('http://localhost:5000', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -93,20 +77,15 @@ const handleSubmit = async (e) => {
       prompt: data.get('prompt'),
     }),
   });
-
   clearInterval(loadInterval);
-  messageDiv.innerHTML = ' ';
-
+  messageDiv.innerHTML = '';
   if (response.ok) {
     const data = await response.json();
-    const parsedData = data.bot.trim(); // trims any trailing spaces/'\n'
-
+    const parsedData = data.bot.trin();
     typeText(messageDiv, parsedData);
   } else {
     const err = await response.text();
-
-    messageDiv.innerHTML = 'Something went wrong';
-    alert(err);
+    messageDiv.innerHTML = 'Something went wrong...';
   }
 };
 
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import { Configuration, OpenAIApi } from 'openai';
 dotenv.config();
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_SECRET_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.get('/', async (req, res) => {
   res.status(200).send({
-    message: 'Hello from CodeX!',
+    message: 'Hello my chatting buddy',
   });
 });
 
@@ -27,21 +27,24 @@ app.post('/', async (req, res) => {
     console.log('prompt', prompt);
     const response = await openai.createCompletion({
       model: 'text-davinci-003',
-      prompt: prompt,
-      temperature: 0, // Higher values means the model will take more risks.
-      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+      prompt: `${prompt}`,
+      temperature: 0.5,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      // stop: ["\"\"\""],
     });
-
+    console.log('res', response.data.choices[0].text);
     res.status(200).send({
-      bot: response.data.choices[0].text,
+      bot: response?.data?.choices[0]?.text,
     });
   } catch (error) {
-    console.error(error);
-    res.status(500).send(error || 'Something went wrong');
+    console.log('error', error);
+    res.status(500).send({ error });
   }
 });
 
-app.listen(5000, () => console.log('Server started on http://localhost:5000'));
+app.listen(5000, () =>
+  console.log('server is running on http://localhost:5000 port')
+);
